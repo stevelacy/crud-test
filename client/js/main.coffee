622e0ad3adf9339d76ae90a4d27346a6cb274c62
Steve Lacy <me@slacy.me>
2013-12-18 19:13:25 -0700

    updates

--- client/js/main.coffee
+++ client/js/main.coffee
@@ -1,4 +1,4 @@
-define ['/js/Router.js', 'models/Item', 'views/Main', 'views/Banner'], (Router, Item, Main, Banner) ->
+define ['app/Router', 'models/Item', 'views/Main', 'views/Banner'], (Router, Item, Main, Banner) ->
 	###
 	test = new Item 
 		title: "test from backbone "
--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,5 @@
 (function() {
-  define(['/js/Router.js', 'models/Item', 'views/Main', 'views/Banner'], function(Router, Item, Main, Banner) {
+  define(['app/Router', 'models/Item', 'views/Main', 'views/Banner'], function(Router, Item, Main, Banner) {
     /*
     	test = new Item 
     		title: "test from backbone "

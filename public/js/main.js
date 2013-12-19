c48d880c6abe150fd930471bd8b0383bbe1d587b
Steve Lacy <me@slacy.me>
2013-12-18 19:06:44 -0700

    updates

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,5 @@
 (function() {
-  define(['app/Router', 'models/Item', 'views/Main', 'views/Banner'], function(Router, Item, Main, Banner) {
+  define(['/js/Router.js', 'models/Item', 'views/Main', 'views/Banner'], function(Router, Item, Main, Banner) {
     /*
     	test = new Item 
     		title: "test from backbone "
